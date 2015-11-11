1d2b09ce842c2d2d4bf215206948e7a5bf008420
omerblink <omerblink@gmail.com>
2015-11-11 19:17:17 +0500

    Fixed main page condition

--- index.php
+++ index.php
@@ -37,52 +37,60 @@
   		add_submenu_page('atq_main', 'Quotes', 'Quotes', 'manage_options', 'quotes', array($this,'atq_main'));
   		add_submenu_page('atq_main', 'Clients', 'Clients', 'manage_options', 'clients', array($this,'atq_main'));
   		add_submenu_page('atq_main', 'Staff Member', 'Staff Member', 'manage_options', 'staff_member', array($this,'atq_main'));
-       }
+   }
   	// Main Page
 
-  	function atq_main() {
-  		?>
-  		<div class="wrap">
-  			<?php
-  		//Requestig Appropriate object
-  			require_once $this->page . '.php';
-  			$obj = new $this->page;
-            // User HTTP request for method
-  			$action = filter_input(INPUT_GET, 'action');
+   function atq_main() {
+    ?>
+    <div class="wrap">
+     <?php
 
-  			if (!isset($action)) {
-  				$action = 'init';
-  			}
+     if ($this->page == 'atq_main') {
+      echo 'Do something...';
+    } else {
+      //Requestig Appropriate object
+      require_once $this->page . '.php';
+      $obj = new $this->page;
 
-  			$obj->$action();
-  			?>
-  			
-  		</div>
-  		<?php
-  	}
+      // User HTTP request for method
+      $action = filter_input(INPUT_GET, 'action');
+
+      if (!isset($action)) {
+        $action = 'init';
+      }
+
+      $obj->$action();
+    }
+
+
+    ?>
+
+  </div>
+  <?php
+}
   	// Notifications
-  	public function notify($module) {
-  		$msg = filter_input(INPUT_GET, 'update');
-  		$settings = filter_input(INPUT_GET, 'settings-updated');
-  		if (isset($msg)) {
-  			echo '<div id="message" class="updated notice notice-success is-dismissible"><p>' . $module . ' ' . $msg . ' successfully!</p><button type="button" class="notice-dismiss"><span class="screen-reader-text">Dismiss this notice.</span></button></div>';
-  		} else if (isset($settings)) {
-  			echo '<div id="message" class="updated notice notice-success is-dismissible"><p>' . $module . ' updated successfully!</p><button type="button" class="notice-dismiss"><span class="screen-reader-text">Dismiss this notice.</span></button></div>';
-  		}
-  	}
+public function notify($module) {
+  $msg = filter_input(INPUT_GET, 'update');
+  $settings = filter_input(INPUT_GET, 'settings-updated');
+  if (isset($msg)) {
+   echo '<div id="message" class="updated notice notice-success is-dismissible"><p>' . $module . ' ' . $msg . ' successfully!</p><button type="button" class="notice-dismiss"><span class="screen-reader-text">Dismiss this notice.</span></button></div>';
+ } else if (isset($settings)) {
+   echo '<div id="message" class="updated notice notice-success is-dismissible"><p>' . $module . ' updated successfully!</p><button type="button" class="notice-dismiss"><span class="screen-reader-text">Dismiss this notice.</span></button></div>';
+ }
+}
 
   	 // Tables queries for database
-  	public function install_tables() {
+public function install_tables() {
 
         // Queries to create tables
-  		$staff_member_table = "CREATE TABLE $this->staff_member_tbl (
-  			staff_id INT(5) NOT NULL AUTO_INCREMENT,
-  			staff_name VARCHAR(100) NOT NULL,
-  			staff_email VARCHAR(256) NOT NULL,
-  			staff_position VARCHAR(256) NOT NULL,
-  			staff_contactno INT(50) NOT NULL,
-  			PRIMARY KEY (staff_id)
-  			) COLLATE = 'utf8_general_ci', ENGINE = 'InnoDB';";
+  $staff_member_table = "CREATE TABLE $this->staff_member_tbl (
+   staff_id INT(5) NOT NULL AUTO_INCREMENT,
+   staff_name VARCHAR(100) NOT NULL,
+   staff_email VARCHAR(256) NOT NULL,
+   staff_position VARCHAR(256) NOT NULL,
+   staff_contactno INT(50) NOT NULL,
+   PRIMARY KEY (staff_id)
+   ) COLLATE = 'utf8_general_ci', ENGINE = 'InnoDB';";
 
 require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 dbDelta($staff_member_table);
